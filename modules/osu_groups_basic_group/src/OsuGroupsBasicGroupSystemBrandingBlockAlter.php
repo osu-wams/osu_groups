ac3dfe702e367f1d19352b55660fbc4368e8db94
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-05-05 16:10:41 -0700

    Make changes to form alter to work for any group type that has our field. Update documentation blocks for variable type. Update prernder call back class to work for Group pages as well as nodes.

--- modules/osu_groups_basic_group/src/OsuGroupsBasicGroupSystemBrandingBlockAlter.php
+++ modules/osu_groups_basic_group/src/OsuGroupsBasicGroupSystemBrandingBlockAlter.php
@@ -4,8 +4,8 @@ namespace Drupal\osu_groups_basic_group;
 
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Render\Element\RenderCallbackInterface;
+use Drupal\group\Entity\Group;
 use Drupal\node\Entity\Node;
-use Drupal\osu_groups\OsuGroupsHandler;
 
 /**
  * Provides a trusted callback to alter the system branding block.
@@ -25,20 +25,39 @@ class OsuGroupsBasicGroupSystemBrandingBlockAlter implements RenderCallbackInter
     CacheableMetadata::createFromRenderArray($build)
       ->addCacheContexts(['url.path'])
       ->applyTo($build);
+
+    /** @var \Drupal\osu_groups\OsuGroupsHandler $osu_groups */
+    $osu_groups = \Drupal::service('osu_groups.group_handler');
+    /** @var \Drupal\osu_groups_basic_group\OsuGroupsBasicGroupHandler $osu_groups_basic_group */
+    $osu_groups_basic_group = \Drupal::service('osu_groups_basic_group.group_handler');
+
     if (preg_match('/node\/(\d+)/', $path, $matches)) {
       $node = Node::load($matches[1]);
-      $osu_groups = \Drupal::service('osu_groups.group_handler');
-      $osu_groups_basic_group = \Drupal::service('osu_groups_basic_group.group_handler');
       $group_content = $osu_groups->getGroupContentFromNode($node);
       if ($group_content) {
         $group_name = $osu_groups->getGroupNameFromNode($node);
+        # Set the group name in the site branding block.
+        $build['content']['site_name']['#markup'] = $group_name;
         $group_landing_page = $osu_groups_basic_group->getGroupLandingNode($group_content->getGroup());
         if (!is_null($group_landing_page)) {
           $group_landing_page->toUrl();
           $group_landing_page_path = $group_landing_page->toUrl()->toString();
-          $build['content']['site_name']['#markup'] = $group_name;
+          # Set the path for the site branding block.
           $build['content']['site_path']['#uri'] = $group_landing_page_path;
         }
+      }
+    }
+    elseif (preg_match('/group\/(\d+)/', $path, $matches)) {
+      $group = Group::load($matches[1]);
+      $group_name = $osu_groups->getGroupnameFromGroup($group);
+      # Set the group name in the site branding block.
+      $build['content']['site_name']['#markup'] = $group_name;
+      $group_landing_page = $osu_groups_basic_group->getGroupLandingNode($group);
+      if (!is_null($group_landing_page)) {
+        $group_landing_page->toUrl();
+        $group_landing_page_path = $group_landing_page->toUrl()->toString();
+        # Set the path for the site branding block.
+        $build['content']['site_path']['#uri'] = $group_landing_page_path;
       }
     }
     return $build;
