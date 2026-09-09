a244761fe39c9dfd15d1d4e81e9ea57d2a0d6c9f
Matthew Brabham <matthew.brabham@oregonstate.edu>
2026-09-08 17:11:40 -0700

    Moving more hooks to OOP

---
+++ modules/osu_groups_basic_group/src/Hook/OsuGroupsBasicGroupPathautoHooks.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\osu_groups_basic_group\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Menu\MenuLinkManagerInterface;
+use Drupal\osu_groups\OsuGroupsHandler;
+use Drupal\pathauto\PathautoPatternInterface;
+
+/**
+ *
+ */
+class OsuGroupsBasicGroupPathautoHooks {
+
+  public function __construct(
+    private readonly OsuGroupsHandler $osuGroupsHandler,
+    private readonly MenuLinkManagerInterface $menuLinkManager,
+  ) {}
+
+  /**
+   * Implements hook_pathauto_pattern_alter().
+   *
+   * Using Patch from https://www.drupal.org/project/group/issues/2774827
+   *
+   * Update pages added to a group not in a menu with
+   *   "/[node:group:url:path]/[node:title]"
+   *
+   * Update pages that are in the Group menu and are the top level link with
+   *   "/[node:group:url:path]/[node:title]"
+   */
+  #[Hook('pathauto_pattern_alter')]
+  public function pathautoPatternAlter(PathautoPatternInterface $pattern, array $context) {
+    if ($context['module'] === 'node' && $context['op'] === 'update') {
+      $node = $context['data']['node'];
+      $group_content = $this->osuGroupsHandler->getGroupContentFromNode($node);
+
+      if ($group_content) {
+        $menu_links = $this->menuLinkManager->loadLinksByRoute('entity.node.canonical', ['node' => $node->id()]);
+
+        if (empty($menu_links)) {
+          $pattern->setPattern('/[node:group:url:path]/[node:title]');
+        }
+        else {
+          $menu_link = reset($menu_links);
+          $parent = $menu_link->getParent();
+
+          if (empty($parent)) {
+            $pattern->setPattern('/[node:group:url:path]/[node:title]');
+          }
+        }
+      }
+    }
+  }
+
+}
---
+++ modules/osu_groups_basic_group/src/Hook/OsuGroupsBasicGroupThemeHooks.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\osu_groups_basic_group\Hook;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\osu_groups\OsuGroupsHandler;
+
+/**
+ * OSU Groups Basic Group Hooks.
+ */
+class OsuGroupsBasicGroupThemeHooks {
+
+  use StringTranslationTrait;
+
+  public function __construct(
+    protected readonly RouteMatchInterface $routeMatch,
+    protected readonly OsuGroupsHandler $osuGroupsHandler,
+    protected readonly ConfigFactoryInterface $configFactory,
+  ) {}
+
+  /**
+   * Implements hook_preprocess_HOOK().
+   */
+  #[Hook('preprocess_menu__group_menu')]
+  public function preprocessGroupMenu(&$variables) {
+    // Add an ID to the menu ul.
+    $variables['attributes']['id'] = 'group-content-menu';
+  }
+
+  /**
+   * Implements hook_preprocess_html().
+   */
+  #[Hook('preprocess_html')]
+  public function preprocessHtml(&$variables) {
+    $node = $this->routeMatch->getParameter('node');
+
+    if ($node) {
+      $group_content = $this->osuGroupsHandler->getGroupContentFromNode($node);
+      $group_node_auto_title = $this->configFactory
+        ->getEditable('group.settings')->get('osu_groups_page_title');
+
+      if ($group_content && $group_node_auto_title) {
+        /** @var \Drupal\group\Entity\Group $group */
+        $group = $group_content->getGroup();
+        // On Group Content Pages insert Group name after node name.
+        $group_name = $this->osuGroupsHandler->getGroupnameFromGroup($group);
+        $page_title_array = explode(' | ', (string) $variables['head_title']['title']);
+        array_splice($page_title_array, 1, 0, $group_name);
+        $variables['head_title']['title'] = implode(' | ', $page_title_array);
+      }
+    }
+  }
+
+  /**
+   * Implements hook_preprocess_HOOK().
+   */
+  #[Hook('preprocess_page_title')]
+  public function preprocessPageTitle(&$variables) {
+    // If we are on a group entity and on the display of it, hide the title.
+    if ($this->routeMatch->getParameter('group')
+      && $this->routeMatch->getRouteName() === 'entity.group.canonical') {
+      $variables['title_attributes']['class'][] = 'hidden';
+    }
+  }
+
+}
